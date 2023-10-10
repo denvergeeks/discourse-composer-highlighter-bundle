28c154590b33ff731a3f2c50831ac53e09d539a1
denvergeeks <jen@denvergeeks.com>
2023-10-09 19:54:50 -0600

    Update api-setup.js

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -2,9 +2,9 @@ import { apiInitializer } from "discourse/lib/api";
 import loadScript from "discourse/lib/load-script";
 import I18n from "I18n";
 
-async function applyDelete(element) {
-  const delete = element.querySelectorAll("del");
-  if (!delete.length) {
+async function applyDeleted(element) {
+  const deleted = element.querySelectorAll("del");
+  if (!deleted.length) {
     return;
   }
 }
@@ -13,16 +13,16 @@ export default apiInitializer("0.11.1", (api) => {
   const { iconNode } = require("discourse-common/lib/icon-library");
   let icon = iconNode("highlighter");
   const currentLocale = I18n.currentLocale();
-  // I18n.translations[currentLocale].js.delete_button_title = I18n.t(themePrefix("composer_delete_button_title"));
-  // I18n.translations[currentLocale].js.composer.delete_button_text = I18n.t(themePrefix("composer_delete_button_text"));
-  I18n.translations[currentLocale].js.delete_button_title = "Delete Text";
+  // I18n.translations[currentLocale].js.deleted_button_title = I18n.t(themePrefix("composer_deleted_button_title"));
+  // I18n.translations[currentLocale].js.composer.deleted_button_text = I18n.t(themePrefix("composer_deleted_button_text"));
+  I18n.translations[currentLocale].js.deleted_button_title = "Delete Text";
   I18n.translations[currentLocale].js.composer.this = "this";
-  // I18n.translations[currentLocale].js.composer.delete_button_text = "Delete Text";
+  // I18n.translations[currentLocale].js.composer.deleted_button_text = "Delete Text";
 
   api.modifyClass("controller:composer", {
-    pluginId: "delete",
+    pluginId: "deleted",
     actions: {
-      deleteButton() {
+      deletedButton() {
         this.get("toolbarEvent").applySurround("\n" + `<del>` + "\n</del>\n");
       },
     },
@@ -31,14 +31,14 @@ export default apiInitializer("0.11.1", (api) => {
   // add button to the toolbar
   api.onToolbarCreate((toolbar) => {
     toolbar.addButton({
-      id: "composer_delete_button",
+      id: "composer_deleted_button",
       group: "extras",
       icon: "highlighter",
       shortcut: "D",
       preventFocus: true,
       trimLeading: true,
-      title: "delete_button_title",
-      // perform: e => e.applySurround('<span>[wrap=delete]', '[/wrap]</span>', 'this')
+      title: "deleted_button_title",
+      // perform: e => e.applySurround('<span>[wrap=deleted]', '[/wrap]</span>', 'this')
       perform: (e) => e.applySurround("<del>", "</del>", "this"),
     });
   });
@@ -46,8 +46,8 @@ export default apiInitializer("0.11.1", (api) => {
   api.decorateCookedElement(
     async (elem, helper) => {
       const id = helper ? `post_${helper.getModel().id}` : "composer";
-      applyDelete(elem, id);
+      applyDeleted(elem, id);
     },
-    { id: "wrap-delete" }
+    { id: "wrap-deleted" }
   );
 });
